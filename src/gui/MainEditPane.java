d49ab36ebabd6361c4248b53520eb7d8e9bef6cb
Ofek <ofekwit@gmail.com>
2014-09-09 15:08:31 +1200

    intial gui made

---
+++ src/gui/MainEditPane.java
@@ -0,0 +1,33 @@
+package gui;
+
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+import javax.swing.JInternalFrame;
+import javax.swing.JPanel;
+
+import uk.co.caprica.vlcj.player.MediaPlayerFactory;
+import uk.co.caprica.vlcj.player.embedded.EmbeddedMediaPlayer;
+
+import javax.swing.JTabbedPane;
+
+import java.awt.BorderLayout;
+
+public class MainEditPane extends JPanel{
+public MainEditPane () {
+		this.setPreferredSize(new Dimension(1000, 150));
+//		JInternalFrame frame = new JInternalFrame();
+//		
+//		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+//		this.getRootPane().add(tabbedPane, BorderLayout.NORTH);
+//		
+//		JPanel panel = new JPanel();
+//		tabbedPane.addTab("New tab", null, panel, null);
+//		
+//		JPanel panel_1 = new JPanel();
+//		tabbedPane.addTab("New tab", null, panel_1, null);
+//	    frame.setVisible(true);
+}
+}
---
+++ src/gui/VideoPanelPlaceHolder.java
@@ -0,0 +1,23 @@
+package gui;
+
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.JPanel;
+
+import uk.co.caprica.vlcj.player.MediaPlayerFactory;
+import uk.co.caprica.vlcj.player.embedded.EmbeddedMediaPlayer;
+
+public class VideoPanelPlaceHolder extends JPanel{
+public VideoPanelPlaceHolder () {
+		this.setPreferredSize(new Dimension(800, 400));
+//		MediaPlayerFactory mediaPlayerFactory = new MediaPlayerFactory();
+//		Canvas c = new Canvas();
+//		c.setBackground(Color.black);
+//		c.setPreferredSize(new Dimension(800,400));
+//		EmbeddedMediaPlayer mediaPlayer = mediaPlayerFactory.newEmbeddedMediaPlayer();
+//		mediaPlayer.setVideoSurface(mediaPlayerFactory.newVideoSurface(c));
+//		this.add(c);
+}
+}
